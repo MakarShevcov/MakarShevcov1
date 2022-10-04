3efe1345a5915afc220ed4af25ec3d3303ebea1b
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-04 20:22:15 +0300

    Add files via upload

---
+++ ДЗ_2.2.py
@@ -0,0 +1,11 @@
+with open("input.txt", "r") as f:
+    a = f.readlines()
+    a = list(map(lambda x: x.split(), a))
+print(a)
+b = int(input())
+x = ''
+for i in range(len(a)):
+     x += str(i % b) + ' ' + str(a[i][0]) + "\n"
+print(x)
+with open("output.txt", "w") as f:
+    f.writelines(x)
