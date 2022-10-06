ddac2b9f4810490a25735ccc83f7e4b5d313500a
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-06 20:24:49 +0300

    Update ДЗ_2.7.py

--- ДЗ_2.7.py
+++ ДЗ_2.7.py
@@ -1,15 +1,9 @@
 with open("input.txt", "r") as f:
     a = f.readlines()
-    a = list(map(lambda x: x.split(), a))
-b = len(a)
-x = []
-for i in range(len(a)):
-     x += [[i % b, a[i]]]
-x1 = sorted(x, key = lambda i: -i[0] )
+    a[len(a)-1] += "\n"
+    a1 = a[::-1]
 k = ''
-for i in range(len(x1)):
-    for j in range(len(x1[i][1])):
-        k += str(x1[i][1][j]) + ' '
-    k += "\n"
+for i in range(len(a1)):
+    k += a1[i]
 with open("output.txt", "w") as f:
     f.writelines(k)
