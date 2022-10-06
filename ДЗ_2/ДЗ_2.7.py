33bcb87b27764368521be0706d1ad7c8cd53e112
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-06 21:16:30 +0300

    Update ДЗ_2.7.py

--- ДЗ_2/ДЗ_2.7.py
+++ ДЗ_2/ДЗ_2.7.py
@@ -1,12 +1,9 @@
-with open("input.txt", "r", encoding="utf-8") as f:
-    a = list(map(lambda x: x.rstrip().split(), f.readlines()))
-y1 = ''
-for i in range(9, 12):
-    k = []
-    for j in range(len(a)):
-        if int(a[j][2]) == i:
-            k += [int(a[j][3])]
-    y = sorted(k,key = lambda x: -x)
-    y1 += str(y[0]) + ' '
+with open("input.txt", "r") as f:
+    a = f.readlines()
+    a[len(a)-1] += "\n"
+    a1 = a[::-1]
+k = ''
+for i in range(len(a1)):
+    k += a1[i]
 with open("output.txt", "w") as f:
-    f.writelines(y1)    
+    f.writelines(k)
