2ad736f809e5869a18cd4a333a97e7ea99027744
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-06 21:18:51 +0300

    Delete ДЗ_2.2.py

--- ДЗ_2.2.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt", "r") as f:
-    a = f.readlines()
-    a = list(map(lambda x: x.split(), a))
-b = int(input())
-x = ''
-for i in range(len(a)):
-     x += str(i % b) + ' ' + str(a[i][0]) + "\n"
-with open("output.txt", "w") as f:
-    f.writelines(x)
