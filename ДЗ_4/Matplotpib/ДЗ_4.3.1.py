850595f0888e4e6905ff57352c82da32887f7b25
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-12 10:49:21 +0300

    Update ДЗ_4.3.1.py

--- ДЗ_4/Matplotpib/ДЗ_4.3.1.py
+++ ДЗ_4/Matplotpib/ДЗ_4.3.1.py
@@ -3,15 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['font.size'] = 16
 plt.figure(figsize=(7,7))
-plt.title(r"$y = e^(-x*sin(x))$")
-plt.ylabel("ось Y")
-plt.xlabel(r"ось X")
 x = np.arange(10, 100, 0.01)
 y = np.exp(-x * np.sin(x))
 mu = np.exp(-x * np.sin(x))
 sigma = np.abs(mu)**0.5
-plt.grid(which='major', axis='both', alpha=1)
-plt.grid(which='minor', axis='both', alpha=0.5)
 plt.legend()
 plt.savefig('example.png')
 plt.plot(x,y, 'r', label='Красные треугольники')
