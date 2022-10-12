8767dae316e7268c9e1310b38ef13d9ac0dd6d55
MakarShevcov <114802475+MakarShevcov@users.noreply.github.com>
2022-10-12 12:02:25 +0300

    Create ДЗ_4.6.1.py

---
+++ ДЗ_4/Matplotpib/ДЗ_4.6.1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['font.size'] = 16
+plt.figure(figsize=(7,7))
+plt.title(r"$y = e^(x*sin(x))$")
+plt.ylabel("ось Y")
+plt.xlabel(r"ось X")
+x = np.arange(10, 100, 1)
+y = np.exp(x * np.sin(x))
+mu = np.exp(x * np.sin(x))
+sigma = np.abs(mu)**0.5
+plt.grid(which='major', axis='both', alpha=1)
+plt.grid(which='minor', axis='both', alpha=0.5)
+plt.minorticks_on()
+plt.legend()
+plt.savefig('example.png')
+plt.plot(x,y, 'r', label='Красные треугольники')
+plt.show()
